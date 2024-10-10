19246a52d5a435c95be660b9874c3447a98ee512
Denis <denis.kiselkov@list.ru>
2024-10-10 19:16:23 +0300

    fix 2 main(2)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "test.h"
 
 int find_sum(int x, int y) {
     std::cout << " txt from find_sum " << std::endl;
     return x + y;
+}
+
+int main() {
+    std::cout << " txt from main " << std::endl;
+	test_find_sum();
+	std::cout << "All tests passed!" << std::endl;
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 #include <cassert> // для assert
-#include "main.cpp" // Подключаем основной файл
+ // Подключаем основной файл
 
 void test_find_sum() {
     std::cout << " txt from test_find_sum " << std::endl;
     assert(find_sum(2, 3) == 5); // если false прерывает выполнение
     assert(find_sum(-1, 1) == 0);
     assert(find_sum(0, 0) == 0);
-}
-
-int main() {
-    std::cout << " txt from test.main " << std::endl;
-    test_find_sum();
-    std::cout << "All tests passed!" << std::endl;
-    return 0;
 }

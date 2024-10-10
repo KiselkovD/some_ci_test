dd836c3c5105000ac10243e7a1199f38f77cfc76
Denis <denis.kiselkov@list.ru>
2024-10-10 18:59:33 +0300

    fix 2 main

--- main.cpp
+++ main.cpp
@@ -4,8 +4,3 @@ int find_sum(int x, int y) {
     std::cout << " txt from find_sum " << std::endl;
     return x + y;
 }
-
-int main() {
-    std::cout << " txt from main " << std::endl;
-    return 0;
-}
